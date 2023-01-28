a66a560fce6b9f1e1224c58f2191f55f9ce60332
brivillasan <brivillayh@gmail.com>
2023-01-28 21:38:58 +0900

    [tax]へメソッド名称を修正いたしました。 ご確認のほど、よろしくお願いいたします。

--- src/Review01.java
+++ src/Review01.java
@@ -6,12 +6,12 @@ public class Review01 {
 
         int nedan = 1500;
         int zeiritu = 8;
-        int result = taxMethod(nedan,zeiritu);
+        int result = tax(nedan,zeiritu);
 
         System.out.println(nedan + "円の商品の税込価格は" + (result+nedan) + "円(消費税は" + result + "円)です。");
     }
 
-    public static int taxMethod(int nedan,int zeiritu) {
+    public static int tax(int nedan,int zeiritu) {
         int result = nedan * zeiritu / 100;
         return result;
     }
